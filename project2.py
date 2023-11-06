748939839ad3ee5269bcba465690c244f59866bf
obinnakalu135 <77299792+obinnakalu135@users.noreply.github.com>
2023-11-05 18:18:01 -0800

    Update project2.py

--- project2.py
+++ project2.py
@@ -1,62 +1,119 @@
-import numpy as np
+import cmath
+import math
 
 def pad_with_zeros(vector, length):
     n = len(vector)
-    padded_vector = np.pad(vector, (0, length - n), mode='constant')
+    padded_vector = vector + [0] * (length - n)
     return padded_vector
 
+
+def round_complex_number(num):
+    # Check if real or imaginary parts are within 0.0001 of an integer
+    real = round(num.real) if abs(num.real % 1) < 0.0001 else round(num.real, 4)
+    imag = round(num.imag) if abs(num.imag % 1) < 0.0001 else round(num.imag, 4)
+    
+    # Omit 0 real part or 0 imaginary part
+    if real != 0:
+        if imag == 0:
+            return f"{real}"
+        elif imag == 1:
+            return f"{real}j"
+        else:
+            return f"{real}+{imag}j"
+    elif imag != 0:
+        if imag == 1:
+            return "j"
+        else:
+            return f"{imag}j"
+    else:
+        return "0"
+
+
 def fft(x):
-    x = np.asarray(x, dtype=complex)
     n = len(x)
-    next_pow_2 = int(2 ** np.ceil(np.log2(n)))  # Find the next power of 2
-    
+    next_pow_2 = int(2 ** math.ceil(math.log2(n)))  # Find the next power of 2
+
     # Pad zeros to x to make its length a power of 2
-    padded_x = np.pad(x, (0, next_pow_2 - n), 'constant')
-    
-    if next_pow_2 <= 2:
+    padded_x = pad_with_zeros(x, next_pow_2)
+
+    if next_pow_2 == 1:
         return padded_x
-    
+
+    w = [cmath.exp(-2j * cmath.pi * i / next_pow_2) for i in range(next_pow_2)]
     even = fft(padded_x[0::2])
     odd = fft(padded_x[1::2])
-    
-    w = np.exp(-2j * np.pi * np.arange(next_pow_2) / next_pow_2)
-    fft_result = np.concatenate([even + w[:next_pow_2 // 2] * odd, even + w[next_pow_2 // 2:] * odd])
-    
-    return fft_result
+
+    result = [0] * next_pow_2
+    for i in range(next_pow_2 // 2):
+        result[i] = even[i] + w[i] * odd[i]
+        result[i + next_pow_2 // 2] = even[i] - w[i] * odd[i]
+
+    return result
+
 
 def ifft(x):
-    x = np.asarray(x, dtype=complex)
     n = len(x)
-    
-    if n <= 1:
+
+    if n == 1:  # Base case for recursion
         return x
-    
+
+    w = [cmath.exp(2j * cmath.pi * i / n) for i in range(n)]
     even = ifft(x[0::2])
     odd = ifft(x[1::2])
-    
-    w = np.exp(2j * np.pi * np.arange(n) / n)
-    ifft_result = np.concatenate([even + w[:n // 2] * odd, even + w[n // 2:] * odd])
-    
-    return ifft_result / n
+
+    ifft_result = [0] * n
+    for i in range(n // 2):
+        ifft_result[i] = even[i] + w[i] * odd[i]
+        ifft_result[i + n // 2] = even[i] - w[i] * odd[i]
+
+    return [(num / n) for num in ifft_result]  # Scale the output by 1/n
+
 
 def convolution(x, y):
     n = len(x) + len(y) - 1
-    x_padded = pad_with_zeros(x, n)
-    y_padded = pad_with_zeros(y, n)
-    result = np.fft.ifft(np.fft.fft(x_padded) * np.fft.fft(y_padded))
-    return result[:n]
+    next_pow_2 = int(2 ** math.ceil(math.log2(n)))  # Find the next power of 2
+    x_padded = pad_with_zeros(x, next_pow_2)
+    y_padded = pad_with_zeros(y, next_pow_2)
 
+    fft_x = fft(x_padded)
+    fft_y = fft(y_padded)
 
-#Testing FFT, IFFT, CONV Functions 
+    fft_product = [fft_x[i] * fft_y[i] for i in range(next_pow_2)]  # Use next_pow_2 instead of n
+    convolution_result = ifft(fft_product)
 
-x = [2, 3, -1, 0, 4]
-y = [1, 2, 3, 1, 12]
+    return convolution_result[:n]
+
+# Testing FFT, IFFT, CONV Functions
+x = [0, 1, 0, 1, 0]
+y = [1, 0, 1, 0, 1]
 
 fft_result = fft(x)
 ifft_result = ifft(fft_result)
 convolution_result = convolution(x, y)
 
-print("FFT result:", fft_result)
-print("IFFT result:", ifft_result)
-print("Convolution result:", convolution_result)
+print("FFT result:", [round_complex_number(num) for num in fft_result])
+print("IFFT result:", [round_complex_number(num) for num in ifft_result])
+print("Convolution result:", [round_complex_number(num) for num in convolution_result])
+
+# Problem  1
+
+def multiply_polynomials(polynomials):
+    result = [1]
+    for polynomial in polynomials:
+        result = convolution(result, polynomial)
+    return result
+
+
+def find_coefficients(roots):
+    linear_factors = [[1, -root] for root in roots]
+    polynomial = multiply_polynomials(linear_factors)
+    coefficients = [round_complex_number(num) for num in polynomial]
+    return coefficients
+
+roots = [1.4142, -1.4142, -1]
+
+coefficients = find_coefficients(roots)
+print("Coefficients:", coefficients)
+
+
 
